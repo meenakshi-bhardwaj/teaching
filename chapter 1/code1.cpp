7aeb83fd6454b1b84865afa0fdb894af531f4ab5
meenakshi bhardwaj <meenakshib7158@gmail.com>
2020-05-22 17:44:49 +0530

    this is modified code

---
+++ chapter 1/code1.cpp
@@ -0,0 +1,5 @@
+this is just for checking how to work with git and git hub 
+this is code1 
+----------------------------------------------------
+this is the part im modifying
+-----------------------------------
---
+++ chapter 2/code12.cpp
@@ -0,0 +1,6 @@
+this is just for checking how to work with git and git hub 
+this is code12
+----------------------------------------------------
+
+this is the part im modigying
+------------------------------------------------
